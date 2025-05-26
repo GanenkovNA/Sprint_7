d48481bd51c5bd331b1f49935964ae3cfeebaf41
GanenkovNA <ganenkov.n.a@gmail.com>
2025-05-26 16:58:43 +0300

    Изменил `isCreated` для возможности класть `null`

--- src/main/java/ru/yandex/praktikum/scooter/courier/dto/CourierEntity.java
+++ src/main/java/ru/yandex/praktikum/scooter/courier/dto/CourierEntity.java
@@ -9,6 +9,7 @@ public class CourierEntity {
     private String login;
     private String password;
     private String firstName;
-    private boolean isCreated = false;
+    @Builder.Default
+    private Boolean created = false;
     private Integer id;
 }
