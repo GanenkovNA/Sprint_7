bdbc5dc6dcd57350cb47726022e414b909391148
GanenkovNA <ganenkov.n.a@gmail.com>
2025-05-30 15:02:50 +0300

    `DeleteValidCourierTest` 1. Объединил тесты на проверку статус кода и тела 2. Добавил `@Description` 3. Заменил значение статус кода на константу из `org.apache.http` 4. Удалил метод `verifyResponseWithLog` за ненадобностью

--- src/test/java/ru/yandex/praktikum/scooter_test/courier_test/positive/DeleteValidCourierTest.java
+++ src/test/java/ru/yandex/praktikum/scooter_test/courier_test/positive/DeleteValidCourierTest.java
@@ -1,11 +1,13 @@
 package ru.yandex.praktikum.scooter_test.courier_test.positive;
 
+import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
 import org.junit.Before;
 import org.junit.Test;
 import ru.yandex.praktikum.scooter_test.courier_test.CourierBase;
 
+import static org.apache.http.HttpStatus.SC_OK;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static ru.yandex.praktikum.scooter_test.courier_test.CourierService.deleteCourier;
 
@@ -21,38 +23,28 @@ public class DeleteValidCourierTest extends CourierBase {
         });
     }
 
-    @DisplayName("Проверка кода ответа (`200`)")
+    @DisplayName("Удаление курьера с валидными данными и проверка ответа")
+    @Description("Проверяются следующие параметры: \n" +
+            "Код ответа - " + SC_OK + "\n" +
+            "В теле содержится `ok = true`\n")
     @Test
-    public void deleteValidCourierAndVerifyStatusCode(){
-        Response response = deleteCourier(courier);
-        verifyResponseWithLog(() -> {
-            assertStatusCode(response,
-                    200,
-                    "deleteValidCourierAndCheck");
-        });
-    }
+    public void shouldDeleteValidCourierAndVerifyResponse(){
+        methodTestWithLog(() -> {
+            Response response = deleteCourier(courier);
 
-    @DisplayName("Проверка параметра `ok = true` в ответе")
-    @Test
-    public void deleteValidCourierAndVerifyBodyParameterOk(){
-        Response response = deleteCourier(courier);
-        verifyResponseWithLog(() -> {
+            logger.debug("Отправлен запрос на удаление курьера {}", courier.getLogin());
+
+            assertStatusCode(response,
+                    SC_OK,
+                    "deleteValidCourierAndCheck");
+
             assertBody(response,
                     "ok",
                     equalTo(true),
                     "Ожидался `ok = true`",
                     "deleteValidCourierAndCheck");
-        });
-    }
-
-    private void verifyResponseWithLog(Runnable verify){
-        methodTestWithLog(() -> {
-            logger.debug("Отправлен запрос на удаление курьера {}", courier.getLogin());
-
-            verify.run();
 
             logger.debug("Успешное удаление курьера {}", courier.getLogin());
         });
     }
-
 }
