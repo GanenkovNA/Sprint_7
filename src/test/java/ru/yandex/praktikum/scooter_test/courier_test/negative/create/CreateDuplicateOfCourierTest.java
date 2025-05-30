aa63a0d2e0f1929d91c3316a66c0f04ebc5fa04c
GanenkovNA <ganenkov.n.a@gmail.com>
2025-05-30 17:00:09 +0300

    `CreateDuplicateOfCourierTest` 1. Объединил тесты на проверку статус кода и тела 2. Добавил `@Description` 3. Заменил значения статус кодов на константы из `org.apache.http` 4. Удалил методы `verifyCreateResponseWithLog` и `verifyLoginResponseWithLog` за ненадобностью

--- src/test/java/ru/yandex/praktikum/scooter_test/courier_test/negative/create/CreateDuplicateOfCourierTest.java
+++ src/test/java/ru/yandex/praktikum/scooter_test/courier_test/negative/create/CreateDuplicateOfCourierTest.java
@@ -1,5 +1,6 @@
 package ru.yandex.praktikum.scooter_test.courier_test.negative.create;
 
+import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
 import org.junit.After;
@@ -8,6 +9,7 @@ import org.junit.Test;
 import ru.yandex.praktikum.scooter.courier.dto.CourierEntity;
 import ru.yandex.praktikum.scooter_test.courier_test.CourierBase;
 
+import static org.apache.http.HttpStatus.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static ru.yandex.praktikum.infrastructure.RandomStringGenerator.generateRandomString;
 import static ru.yandex.praktikum.scooter_test.courier_test.CourierService.*;
@@ -16,9 +18,7 @@ import static ru.yandex.praktikum.scooter_test.courier_test.CourierService.*;
 public class CreateDuplicateOfCourierTest extends CourierBase {
     private CourierEntity courierDuplicate;
 
-    private final int EXPECTED_STATUS_CODE_CREATE = 409;
     private final String EXPECTED_MESSAGE_CREATE = "Этот логин уже используется";
-    private final int EXPECTED_STATUS_CODE_LOGIN = 404;
 
     @Before
     public void createValidCourierAndCheckThrowLogin(){
@@ -35,22 +35,21 @@ public class CreateDuplicateOfCourierTest extends CourierBase {
         });
     }
 
-    @DisplayName("Проверка кода ответа (`409`)")
+    @DisplayName("Попытка добавления дубликата курьера и проверка ответа")
+    @Description("Проверяются следующие параметры: \n" +
+            "Код ответа - " + SC_CONFLICT + "\n" +
+            "В теле содержится `message = " + EXPECTED_MESSAGE_CREATE + "`")
     @Test
-    public void createDuplicateOfCourierAndVerifyStatusCode(){
+    public void shouldNotCreateDuplicateOfCourierAndVerify(){
         Response response = addNewCourier(courierDuplicate);
-        verifyCreateResponseWithLog(() -> {
-            assertStatusCode(response,
-                    EXPECTED_STATUS_CODE_CREATE,
-                    getCurrentTestMethod());
-        });
-    }
 
-    @DisplayName("Проверка параметра `message` в ответе")
-    @Test
-    public void createDuplicateOfCourierAndVerifyBodyParameterMessage(){
-        Response response = addNewCourier(courierDuplicate);
-        verifyCreateResponseWithLog(() -> {
+        logger.debug("Отправлен запрос на создание дубликата курьера {}", courierDuplicate.getLogin());
+
+        verifyResponse(() -> {
+            assertStatusCode(response,
+                    SC_CONFLICT,
+                    getCurrentTestMethod());
+
             assertBody(response,
                     "message",
                     equalTo(EXPECTED_MESSAGE_CREATE),
@@ -59,14 +58,19 @@ public class CreateDuplicateOfCourierTest extends CourierBase {
         });
     }
 
-    @DisplayName("Проверка возможности авторизации дубликатом курьера")
+    @DisplayName("Проверка невозможности авторизации дубликатом курьера")
+    @Description("Проверяется код ответа - " + SC_CONFLICT)
     @Test
-    public void loginDuplicateOfCourierAndVerifyStatusCode(){
+    public void shouldNotLoginDuplicateOfCourierAndVerifyStatusCode(){
         addNewCourier(courierDuplicate);
         Response response = loginCourier(courierDuplicate);
-        verifyLoginResponseWithLog(() -> {
+
+        logger.debug("Отправлен запрос на логин дубликата курьера {}", courierDuplicate.getLogin());
+
+
+        verifyResponse(() -> {
             assertStatusCode(response,
-                    EXPECTED_STATUS_CODE_LOGIN,
+                    SC_NOT_FOUND,
                     getCurrentTestMethod());
         });
     }
@@ -74,16 +78,6 @@ public class CreateDuplicateOfCourierTest extends CourierBase {
     @After
     public void cleanUp(){
         safeCleanUp(this::deleteValidCourierAndVerify);
-    }
-
-    private void verifyCreateResponseWithLog(Runnable verify){
-        logger.debug("Отправлен запрос на создание дубликата курьера {}", courierDuplicate.getLogin());
-        verifyResponse(verify);
-    }
-
-    private void verifyLoginResponseWithLog(Runnable verify){
-        logger.debug("Отправлен запрос на логин дубликата курьера {}", courierDuplicate.getLogin());
-        verifyResponse(verify);
     }
 
     private void verifyResponse(Runnable verify){
